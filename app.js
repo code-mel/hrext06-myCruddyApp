285ff71637c012e06d85a3a998f2d48aacc695c8
Melsm650 <wp.developerhelpers@gmail.com>
2018-12-18 14:29:38 -0800

    orderlist page, created and console log shows on index and orders.html

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  // console.log('jQuery loaded');
+  console.log('jQuery loaded');
   //
   // // write to local storage from input when button save clicked
   // $('.btn-submit').on('click', function(){
