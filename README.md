2119f9b9f403b449bc2564bdd1a1391c1af640ca
Melsm650 <wp.developerhelpers@gmail.com>
2018-12-18 14:22:05 -0800

    skeleton for index page which is order page is set

--- README.md
+++ README.md
@@ -4,21 +4,22 @@ Create Read Update Delete using localStorage with JS, HTML and CSS
 
 ## To Do List
 
-### Current Sprint
+### Current Sprint - Coffee shop
+
 (the unchecked boxes are required to finish)
 - [x] Form with input fields
-- [x] Write to local storage
-- [x] Read from local storage
+- [x] Write to local storage// for all orders
+- [x] Read from local storage // review and seen for waiter
 - [x] Edit local storage
     - [ ] What about if we have more than one value?
     - [ ] how do we add multiple values?
         - [ ] maybe use an array?
         - [ ] maybe use multiple keys? create new keyname each time
 
-- [x] Delete local storage
+- [x] Delete local storage // delete button on maker page
     - [x] delete button storage.removeItem()
     - [ ] delete all storage.clear()
-- [x] Display stored value in proper div
+- [x] Display stored value in proper div // on waiter page based on status of order
 
 ### Next Sprint (what are we trying to code)
 Examples: Replace with your own.
--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
-  console.log('jQuery loaded');
-
-  // write to local storage from input when button save clicked
-  $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
-
-    // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
-
-  });
-
-  // delete from local storage when delete button clicked
-  $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
-  });
+  // console.log('jQuery loaded');
+  //
+  // // write to local storage from input when button save clicked
+  // $('.btn-submit').on('click', function(){
+  //   localStorage.setItem('inputFieldValue', $('.text-entry').val());
+  //   var myItemInStorage = localStorage.getItem('inputFieldValue');
+  //   console.log('myItemInStorage', myItemInStorage);
+  //
+  //   // display the value here
+  //   $('.list-display-field').text(myItemInStorage); // ??
+  //
+  // });
+  //
+  // // delete from local storage when delete button clicked
+  // $('.btn-delete').on('click', function(){
+  //   localStorage.removeItem('inputFieldValue');
+  // });
 
 });
